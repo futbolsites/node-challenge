f7b2efbc325004c4f00156972f7923492b0ccf7b
lucas llopis <lucasllopis@MacBook-Pro.local>
2024-06-18 11:39:42 -0300

    add body parser audit fix

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+const bodyParser = require('body-parser');
 import healthRouter from './health/routes';
 
 import 'dotenv/config';
 
 const app = express();
+app.use(bodyParser.json());
 
 const router = express.Router();
 
